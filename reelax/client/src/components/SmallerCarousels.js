f5145ce4f37b7bc6feb04bdee5d5ce0921faf115
Corrine <corrine.sing@gmail.com>
2023-07-01 17:04:01 +0100

    partially working carousels

--- reelax/client/src/components/SmallerCarousels.js
+++ reelax/client/src/components/SmallerCarousels.js
@@ -1,32 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
+import { Link, useParams } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Images from "./Images";
 import '../App.css'
+import MovieDetail from "./MovieDetail";
 
-const SmallerCarousels = () => {
+const SmallerCarousels = ({ movie, genre, findMovieById }) => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = () => {
+            fetch(`/api/movies?genre=${genre}`)
+                .then(response => response.json())
+                .then(data => setMovies(data.slice(0, 9)))
+                .catch(error => console.log('Error fetching movies:', error));
+        };
+        fetchMovies();
+    }, [genre]);
+
 
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 3,
         autoplay: false,
         autoplaySpeed: 1000,
     };
 
+        const MovieDetailWrapper = () => {
+        const {id} = useParams()
+        let foundMovie = findMovieById(id)
+        return <MovieDetail movie={foundMovie}/>
+    }
+
+    const url = "/movies/" + movie.id;
+
+
     return (
         <>
             <div className="content">
-                <h1 className="header">Car Gallery</h1>
+                <h1 className="header">All Movies</h1>
                 <div className="sml-carousel-container">
                     <Slider {...settings}>
-                        {Images.map((item) => (
-                            <div key={item.id}>
-                                <img src={item.src} alt={item.alt} className="img" />
-                                <h5 className="title">{item.title}</h5>
+                        {movies.map((movie) => (
+                            <div>
+                                <Link to={url} element={<MovieDetailWrapper/>}>
+                                    <img
+                                        id="poster"
+                                        alt={movie.title}
+                                        className="img"
+                                        src={"https://image.tmdb.org/t/p/original" + movie.poster}
+                                    />
+                                </Link>
+                                <h5 className="title">{movie.title}</h5>
+                            </div>
+                        ))}
+                    </Slider>
+                </div>
+            </div>
+
+            <div className="content">
+                <h1 className="header">Comedy</h1>
+                <div className="sml-carousel-container">
+                    <Slider {...settings}>
+                        {movies.map((movie) => (
+                            <div>
+                                <img
+                                    id="poster"
+                                    alt={movie.title}
+                                    className="img"
+                                    src={"https://image.tmdb.org/t/p/original" + movie.poster}
+                                />
+                                <h5 className="title">{movie.title}</h5>
                             </div>
                         ))}
                     </Slider>
--- reelax/client/src/containers/MainContainer.js
+++ reelax/client/src/containers/MainContainer.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Request from '../helpers/request'
 import LiveSearch from './LiveSearch'
 import MovieCard from '../components/MovieCard';
-import {BrowserRouter as Router, Route, Routes, useParams} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom'
 import ProfileCard from '../components/ProfileCard';
 import NavBar from './NavBar';
 import MovieDetail from '../components/MovieDetail';
@@ -11,45 +11,45 @@ import MovieSearchCard from '../components/MovieSearchCard';
 import SmallerCarousels from '../components/SmallerCarousels';
 
 
-const MainContainer = ({user, removeUser, onUserLogout, addToWatchList}) => {
-
-    
-    const [movies, setMovies] = useState([])
-    const [users, setUsers] = useState([])
-    const [reviews, setReviews] = useState([])
-    const [movieTitle, setMovieTitle] = useState([])
-    const [searchInput, setSearchInput] = useState("");
-    const [foundMovies, setFoundMovies] = useState("");
+const MainContainer = ({ user, removeUser, onUserLogout, addToWatchList }) => {
 
 
+  const [movies, setMovies] = useState([])
+  const [users, setUsers] = useState([])
+  const [reviews, setReviews] = useState([])
+  const [movieTitle, setMovieTitle] = useState([])
+  const [searchInput, setSearchInput] = useState("");
+  const [foundMovies, setFoundMovies] = useState("");
 
-    useEffect(() => {
-      getMovies();
-      getReviews();
-    }, [])
-    
 
 
-  const getMovies= () => {
-    const request= new Request()
+  useEffect(() => {
+    getMovies();
+    getReviews();
+  }, [])
+
+
+
+  const getMovies = () => {
+    const request = new Request()
     request.get("/api/movies")
-    .then((data) => {
-      setMovies(data)
-    })
+      .then((data) => {
+        setMovies(data)
+      })
   }
 
 
-  const getReviews= () => {
-    const request= new Request()
+  const getReviews = () => {
+    const request = new Request()
     request.get("/api/reviews")
-    .then((data) => {
-      setReviews(data)
-    })
+      .then((data) => {
+        setReviews(data)
+      })
   }
 
   const getMovieByTitle = (title) => {
     const request = new Request();
-    if(title === ''){
+    if (title === '') {
       getMovies();
     }
     if (title) {
@@ -61,8 +61,8 @@ const MainContainer = ({user, removeUser, onUserLogout, addToWatchList}) => {
       getMovies();
     }
   }
-  
-  
+
+
 
   const handleLogout = () => {
     onUserLogout();
@@ -73,117 +73,119 @@ const MainContainer = ({user, removeUser, onUserLogout, addToWatchList}) => {
     const request = new Request()
     const url = "/api/users/" + id;
     request.delete(url)
-    .then (() => {
-      window.location = "/users"
-    })
+      .then(() => {
+        window.location = "/users"
+      })
   }
 
   // const addUser = (user) => {
   //   setUsers([...users, user])
 
   // }
-    
-      // const getMovies = function () {
-      
 
-      //     // const options = {
-      //     //     method: 'GET',
-      //     //     headers: {
-      //     //       accept: 'application/json',
-      //     //       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTJmOTk5MTUyNzFhZmI3NGQxZmJmZjQxMDI2ZWI0YyIsInN1YiI6IjY0OTVhMDU1ODgwNTUxMDBlNzQ0N2FjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lFaWCPsXKZFT5aaHoRbNYKgyNEhOsVtu1AHUUZGVZ1g'
-      //     //     }
-      //     //   };
-      //       const allRequests = []
-      //       for (let i = 1; i <= 1000; i++){
-      //         const newFetch = fetch("http://api.themoviedb.org/3/discover/movie?api_key=7f46651666f1ca68e4cf0cb150551f07&page=" + i)
-      //         .then(response => response.json())
-      //         allRequests.push(newFetch)
-      //       }
-      //         Promise.all(allRequests)
-      //         .then((data) => saveToDb(data))
-      
-      // }
-
-      // const saveToDb =(data) =>{
-      //   for(let result of data){
-
-      //     for(let movie of result.results){
-      //       let newMovie = {
-      //         "title": movie.title,
-      //         "overview": movie.overview,
-      //         "poster": movie.poster_path,
-      //         "genre": 1,
-      //         "release": movie.release_date,
-      //         "backdrop": movie.backdrop_path 
-      //       }
-            
-      //       console.log(newMovie);
-      //       let request = new Request()
-      //     request.post("/api/movies", newMovie)
-      //     }
-      //   }
-
-      // }
+  // const getMovies = function () {
 
 
-    
-    const findMovieById = (id) => {
-      let foundMovie = null;
-      for (let movie of movies){
-        if (movie.id === parseInt(id)){
-          foundMovie = movie
-        }
+  //     // const options = {
+  //     //     method: 'GET',
+  //     //     headers: {
+  //     //       accept: 'application/json',
+  //     //       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTJmOTk5MTUyNzFhZmI3NGQxZmJmZjQxMDI2ZWI0YyIsInN1YiI6IjY0OTVhMDU1ODgwNTUxMDBlNzQ0N2FjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lFaWCPsXKZFT5aaHoRbNYKgyNEhOsVtu1AHUUZGVZ1g'
+  //     //     }
+  //     //   };
+  //       const allRequests = []
+  //       for (let i = 1; i <= 1000; i++){
+  //         const newFetch = fetch("http://api.themoviedb.org/3/discover/movie?api_key=7f46651666f1ca68e4cf0cb150551f07&page=" + i)
+  //         .then(response => response.json())
+  //         allRequests.push(newFetch)
+  //       }
+  //         Promise.all(allRequests)
+  //         .then((data) => saveToDb(data))
+
+  // }
+
+  // const saveToDb =(data) =>{
+  //   for(let result of data){
+
+  //     for(let movie of result.results){
+  //       let newMovie = {
+  //         "title": movie.title,
+  //         "overview": movie.overview,
+  //         "poster": movie.poster_path,
+  //         "genre": 1,
+  //         "release": movie.release_date,
+  //         "backdrop": movie.backdrop_path 
+  //       }
+
+  //       console.log(newMovie);
+  //       let request = new Request()
+  //     request.post("/api/movies", newMovie)
+  //     }
+  //   }
+
+  // }
+
+
+
+  const findMovieById = (id) => {
+    let foundMovie = null;
+    for (let movie of movies) {
+      if (movie.id === parseInt(id)) {
+        foundMovie = movie
       }
-      return foundMovie
     }
-    
-    const MovieDetailWrapper = () => {
-      const {id} = useParams()
-      let foundMovie = findMovieById(id)
-      console.log("foundMovie", foundMovie);
-      return <MovieDetail user={user} movie={foundMovie} addToWatchList={addToWatchList} reviews={reviews}/>
+    return foundMovie
+  }
+
+  const MovieDetailWrapper = () => {
+    const { id } = useParams()
+    let foundMovie = findMovieById(id)
+    console.log("foundMovie", foundMovie);
+    return <MovieDetail user={user} movie={foundMovie} addToWatchList={addToWatchList} reviews={reviews} />
   }
 
 
-    const movieDisplay = movies.map((movie, index) => {
-        return <li key={index}><MovieCard movie={movie} findMovieById={findMovieById} /></li>
-    })
+  const movieDisplay = movies.map((movie, index) => {
+    return <li key={index}><MovieCard movie={movie} findMovieById={findMovieById} /></li>
+  })
 
-    const movieSearchDisplay = movies.map((movie, index) => {
-      return <MovieSearchCard key={index} movie={movie} />
-    })
-    
-    
-    return ( 
-        <div>
-          <div>
-          <Router>
-            <NavBar handleLogout={handleLogout} setSearchInput={setSearchInput}/>
-            <LiveSearch getMovieByTitle={getMovieByTitle} searchInput={searchInput} setSearchInput={setSearchInput}/>
+  const movieSearchDisplay = movies.map((movie, index) => {
+    return <MovieSearchCard key={index} movie={movie} />
+  })
+
+
+  return (
+    <div>
+
+      <div>
+        <Router>
+          <NavBar handleLogout={handleLogout} setSearchInput={setSearchInput} />
+          <LiveSearch getMovieByTitle={getMovieByTitle} searchInput={searchInput} setSearchInput={setSearchInput} />
+
+          <SmallerCarousels movie={movies} findMovieById={findMovieById} />
+
           <Routes>
-
-          <Route path="/" element = {movieDisplay}/>
-          <Route path="/movies/:id" element={<MovieDetailWrapper/>}/>
-          <Route path="/profile" element = {<ProfileCard key={user.id} user={user} handleDelete={handleDelete} reviews={reviews} movies={movies} MovieDetailWrapper={MovieDetailWrapper} getReviews={getReviews}/>}/>
-
+            <Route path="/" element={movieDisplay} />
+            <Route path="/movies/:id" element={<MovieDetailWrapper />} />
+            <Route path="/profile" element={<ProfileCard key={user.id} user={user} handleDelete={handleDelete} reviews={reviews} movies={movies} MovieDetailWrapper={MovieDetailWrapper} getReviews={getReviews} />} />
           </Routes>
-          </Router>
-          </div>
+        </Router>
+      </div>
 
-            <ul>
 
-              {/* <p>this is a movie container</p>
+      <ul>
+
+        {/* <p>this is a movie container</p>
                 {movieDisplay} 
                 {/* {userDisplay}
-                {reviewDisplay} */} 
-            </ul>
+                {reviewDisplay} */}
+      </ul>
 
-          <div>
-            <SmallerCarousels/>
-          </div>
-            
-        </div>
-     );
+      <div>
+      </div>
+
+    </div>
+  );
 }
- 
+
 export default MainContainer;
