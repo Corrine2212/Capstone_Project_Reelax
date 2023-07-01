28ebc25346a5638a478a4f7222a93198b6b9582c
Codeclan Student <codeclanstudent@Codeclans-MacBook-Pro.local>
2023-07-01 23:22:57 +0100

    Backend Filter

--- Capstone-Reelax 2/src/main/java/com/example/Capstone/Reelax/Repositories/MovieRepository.java
+++ Capstone-Reelax 2/src/main/java/com/example/Capstone/Reelax/Repositories/MovieRepository.java
@@ -2,8 +2,20 @@ package com.example.Capstone.Reelax.Repositories;
 
 import com.example.Capstone.Reelax.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByTitleContainingIgnoreCase(String title);
+
+    @Query("SELECT m FROM Movie m WHERE m.release BETWEEN :start AND :end")
+    List<Movie> findMoviesBetweenDates(@Param("start") LocalDate start, @Param("end") LocalDate end);
+
 }
+
+
