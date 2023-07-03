2b55ebacd9b6d7ce6525f3b42d120f7e0b4a57dc
ewangomolka <ewangomolka@gmail.com>
2023-07-03 15:00:31 +0100

    logged in user

--- reelax/client/src/App.js
+++ reelax/client/src/App.js
@@ -27,47 +27,57 @@ function App() {
   const [loggedInUser, setLoggedInUser] = useState(null)
   // const [postToEdit, setPostToEdit] = useState(null)
   // const [posts, setPosts] = useState([])
-  useEffect(() => {
-    getUsers()
-  }, [])
   const getUsers = () => {
     return fetch(baseURL)
-      .then(res => res.json())
-      .then(data => setUsers(data))
+    .then(res => res.json())
+    .then(data => setUsers(data))
   }
+
+  const setUser = (newUser) => {
+    setLoggedInUser(newUser)
+  }
+
   const onSubmitLogin = (searchUser) => { // searchUser is the user that is trying to log in
     console.log('search user', searchUser);
     const workingUser = users.find(
       (user) => // find the user that matches the username or email and password
-        (user.username === searchUser.username || user.email === searchUser.username) &&
-        user.password === searchUser.password
-    );
-    if (workingUser) {
-      setLoggedInUser(workingUser)
-      // localStorage.setItem(‘loggedInUser’, JSON.stringify(workingUser)); // stores the logged in user in local storage
-    } else { // if user doesn’t exist
-      console.log('Authentication failed');
-    }
-  };
-  const onUserLogout = () => {
+      (user.username === searchUser.username || user.email === searchUser.username) &&
+      user.password === searchUser.password
+      );
+      if (workingUser) {
+        setLoggedInUser(workingUser)
+        // localStorage.setItem(‘loggedInUser’, JSON.stringify(workingUser)); // stores the logged in user in local storage
+      } else { // if user doesn’t exist
+        console.log('Authentication failed');
+      }
+    };
+    const onUserLogout = () => {
     setLoggedInUser(null);
-    localStorage.removeItem('loggedInUser') // removes the logged in user from local storage
+    localStorage.removeItem('user') // removes the logged in user from local storage
   }
-
-
-const removeUser = (id) => {
-  const usersToKeep = users.filter(user => user._id !== id)
-  setUsers(usersToKeep);
-}
-
-
-// const handleEditClicked = (id, post) => {
-//   putPost(id, post)
-//   let updatedPostIndex = posts.indexOf(post)
-//   let newPosts = [...posts]
-//   newPosts[updatedPostIndex] = post
-//   setPostToEdit(newPosts)
-// }
+  
+  useEffect(() => {
+    getUsers()
+    const loggedUser = localStorage.getItem("user")
+    if (loggedUser){
+      const foundUser = JSON.parse(loggedUser)
+      setUser(foundUser)
+    }
+  }, [onUserLogout])
+  
+  const removeUser = (id) => {
+    const usersToKeep = users.filter(user => user._id !== id)
+    setUsers(usersToKeep);
+  }
+  
+  
+  // const handleEditClicked = (id, post) => {
+    //   putPost(id, post)
+    //   let updatedPostIndex = posts.indexOf(post)
+    //   let newPosts = [...posts]
+    //   newPosts[updatedPostIndex] = post
+    //   setPostToEdit(newPosts)
+    // }
 // const addPost = (post) => {
 //   setPosts([...posts, post]);
 // }
